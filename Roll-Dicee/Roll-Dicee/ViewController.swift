e153c60425f289776ae3616551bba315b9c24ad7
Ramin Keshavarzi <raminkeshavarzi@organiplace.com>
2021-03-07 11:27:04 +0100

    added functional logic

--- Roll-Dicee/Roll-Dicee/ViewController.swift
+++ Roll-Dicee/Roll-Dicee/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//Interface Builder Outlet
+    
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var diceImgViewOne: UIImageView!
+    @IBOutlet weak var diceImgViewTwo: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    //Override Function
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+//Functions
+    @IBAction func rollerFunction(_ sender: Any) {
+        diceImgViewOne.image = #imageLiteral(resourceName: "DiceSix")
+        diceImgViewOne.alpha = 0.9
+        diceImgViewTwo.image = #imageLiteral(resourceName: "DiceSix")
+        diceImgViewTwo.alpha = 0.9
+        label.text = "Kudos 🥳"
+        logoImg.alpha = 1
+    }
 }
 
